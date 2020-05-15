b2c5ed8e400066faa2dd3517925a9b55e5c81856
Micaella Morales <morales.micaella@gmail.com>
2020-05-14 18:38:38 -0700

    fixed syntax

--- LineSensor.c
+++ LineSensor.c
@@ -20,30 +20,30 @@ void sensor_setup() {
 
 void detect_line() {
     // move straight: left on white, center on black, right on white 
-    if((digitalRead(IR_L) == 0) && (digitalRead(IR_C) == 1)&& (digitalRead(IR_R)== 0)) {
+    if(digitalRead(IR_L) == 0 && digitalRead(IR_C) == 1 && digitalRead(IR_R)== 0) {
         straight = 1; 
         right = 0; 
         left = 0;
     }
     // turn left: left on black, center on white, right on white
-    else if((digitalRead(IR_L == 1)) && (digitalRead(IR_C == 0))&& (digitalRead(IR_C == 0)) {
+    else if(digitalRead(IR_L) == 1 && digitalRead(IR_C) == 0 && digitalRead(IR_C) == 0) {
         straight = 0; 
         right = 0; 
         left = 1;
     }
     // turn left: left on black, center on black, right on white
-    else if((digitalRead(IR_L == 1)) && (digitalRead(IR_C == 1))&& (digitalRead(IR_C == 0)) {
+    else if(digitalRead(IR_L) == 1 && digitalRead(IR_C) == 1 && digitalRead(IR_C) == 0) {
         straight = 0; 
         right = 0; 
         left = 1;
     }
-    else if((digitalRead(IR_L == 0)) && (digitalRead(IR_C == 0))&& (digitalRead(IR_C == 1)) {
+    else if(digitalRead(IR_L) == 0 && digitalRead(IR_C) == 0 && digitalRead(IR_C) == 1) {
         straight = 0; 
         right = 1; 
         left = 0;
     }
     // turn right:left on white, center on black, right on black
-    else if((digitalRead(IR_L == 0)) && (digitalRead(IR_C == 1))&& (digitalRead(IR_C == 1)) {
+    else if(digitalRead(IR_L) == 0 && digitalRead(IR_C) == 1 && digitalRead(IR_C) == 1) {
         straight = 0; 
         right = 1; 
         left = 0;
