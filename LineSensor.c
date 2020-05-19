2586424a5c9f3fa0bbb520199fa6a248359b915f
Daisy <Daisysanchez202012@gmail.com>
2020-05-19 14:23:47 -0700

    comments

--- LineSensor.c
+++ LineSensor.c
@@ -8,7 +8,7 @@
 
 //BCM: 22, 14, 10
 #define IR_L 6
-#define IR_C 15 
+#define IR_C 15
 #define IR_R 12
 
 void sensor_setup()
@@ -16,30 +16,29 @@ void sensor_setup()
     pinMode(IR_L, INPUT);
     pinMode(IR_C, INPUT);
     pinMode(IR_R, INPUT);
-
 }
 
 PI_THREAD(detect_line)
 {
-     while (1) 
+    while (1)
     {
-        // move forward: left on white, center on black, right on white 
-        if((digitalRead(IR_L) == 0) && (digitalRead(IR_C) == 1)&& (digitalRead(IR_R)== 0))
+        // move forward if left sensor is on white, center sensor is on black, right sensor is on white
+        if ((digitalRead(IR_L) == 0) && (digitalRead(IR_C) == 1) && (digitalRead(IR_R) == 0))
         {
             return move_straight();
         }
-        // turn left: left on black, center on white, right on white
-        else if((digitalRead(IR_L == 1)) && (digitalRead(IR_C == 0))&& (digitalRead(IR_C == 0))
+        // turn left if left sensor is on black, center sensor is on white, left sensor is on white
+        else if((digitalRead(IR_L == 1)) && (digitalRead(IR_C == 0)) (digitalRead(IR_R == 0))
         {
-            return move_left;
+            return move_left();
         }
-        // turn right:left on white, center on black, right on white
-        else if((digitalRead(IR_L == 0)) && (digitalRead(IR_C == 0))&& (digitalRead(IR_C == 1))
+        // turn right if left sensor is on white, center sensor is on white, right sensor is on black
+        else if((digitalRead(IR_L == 0)) && (digitalRead(IR_C == 0))&& (digitalRead(IR_R == 1))
         {
             return move_right();
         }
-        // Stops if all black(1)
-        else ((digitalRead(IR_L == 1)) && (digitalRead(IR_C == 1))&& (digitalRead(IR_C == 1))
+        // Stops if all sensors are black(1)
+        else ((digitalRead(IR_L == 1)) && (digitalRead(IR_C == 1))&& (digitalRead(IR_R == 1))
         {
             return stop();
         }
