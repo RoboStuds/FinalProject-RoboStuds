a96468f18b9eaef4fbbe1059df54c12b43b037a9
Christy Tam <38668397+ctam2@users.noreply.github.com>
2020-05-20 16:30:13 -0700

    Update Motor.h

--- Motor.h
+++ Motor.h
@@ -19,8 +19,8 @@
     void forward(Motor motors[], int num_motors, Arrow arrows);
     void backward(Motor motors[], int num_motors, Arrow arrows);
     void move_straight(Motor right_motor, Motor left_motor, int duty_cycle, Arrow arrows);
-    void move_right(Motor right_motor, Motor left_motor, int duty_cycle, Arrow arrows);
-    void move_left(Motor left_motor, Motor right_motor, int duty_cycle, Arrow arrows);
-    void set_speed(Motor motors[], Motor right_motor, Motor left_motor, int num_motors, int duty_cycle);
+    void move_right(Motor right_motor, Motor left_motor, Arrow arrows);
+    void move_left(Motor left_motor, Motor right_motor, Arrow arrows);
+    void set_speed(Motor motors[], int num_motors, int duty_cycle);
     
 #endif
