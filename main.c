d0e2c161da83409a0a89a2f9597a4fae7ee85556
Micaella Morales <morales.micaella@gmail.com>
2020-05-21 12:14:44 -0700

    updated

--- main.c
+++ main.c
@@ -15,8 +15,8 @@
 #define DISTANCE_KEY 0
 #define POSITION_KEY 1
 
-int reg_speed = 13;
-int gentle_turn_speed = 28;
+int reg_speed = 15;
+int gentle_turn_speed = 30;
 int sharp_turn_speed = 36;
 
 static volatile int global_pos = 0;
