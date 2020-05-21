c2fe5e049a8caea87d154e4f59925d8b426fc52c
Micaella Morales <morales.micaella@gmail.com>
2020-05-21 01:48:41 -0700

    updated

--- main.c
+++ main.c
@@ -160,7 +160,7 @@ int main(void) {
         if (!is_obstacle()) {
             keep_on_track();
 
-            delay(500);
+            delay(1000);
         } else {
             printf("detected obstacle\n");
             stop_motors(FR_MOTOR, FL_MOTOR);
