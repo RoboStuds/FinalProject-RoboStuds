dcff25b8cf83eab5e6a4f2e19b00779e56764636
Micaella Morales <morales.micaella@gmail.com>
2020-05-20 23:56:31 -0700

    testing

--- main.c
+++ main.c
@@ -16,8 +16,8 @@
 #define POSITION_KEY 1
 
 int reg_speed = 20;
-int gentle_turn_speed = 30;
-int sharp_turn_speed = 50;
+int gentle_turn_speed = 40;
+int sharp_turn_speed = 60;
 
 static volatile int global_pos = 0;
 static volatile double global_dist = 0;
